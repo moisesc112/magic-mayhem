e2742fb9abc36ec86be2ecb09e35da1551fb3ef3
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2024-09-18 17:28:27 -0700

    Reset Enemy AI when grabbing enemy from object pool.

--- Assets/Scripts/Enemy/AbstractEnemy.cs
+++ Assets/Scripts/Enemy/AbstractEnemy.cs
@@ -6,32 +6,32 @@ using UnityEngine.AI;
 [RequireComponent(typeof(HealthComponent))]
 public abstract class AbstractEnemy : MonoBehaviour
 {
-    //properties that will be used to reference
-    private Player targetPlayer;
-    private NavMeshAgent agent;
-    public LayerMask isPlayer;
+	//properties that will be used to reference
+	private Player targetPlayer;
+	private NavMeshAgent agent;
+	public LayerMask isPlayer;
 
-    // Public properties
-    public float attackRange;
-    public float attackDamage;
-    public float attackCooldownTimer = 1.5f;
-    public float movementSpeed;
+	// Public properties
+	public float attackRange;
+	public float attackDamage;
+	public float attackCooldownTimer = 1.5f;
+	public float movementSpeed;
 
-    // Private bools
-    private bool inAttackRange= false;
-    private bool onCooldown = false;
+	// Private bools
+	private bool inAttackRange= false;
+	private bool onCooldown = false;
 	bool isAttacking = true;
 
-    [SerializeField] DistancePollObject[] distancePollInfos;
-    float _distancePollInterval = 0.1f;
+	[SerializeField] DistancePollObject[] distancePollInfos;
+	float _distancePollInterval = 0.1f;
 
-    private void Awake()
-    {
-        // Set up the agent and target
-        agent = GetComponent<NavMeshAgent>();
-        agent.speed = movementSpeed;
+	private void Awake()
+	{
+		// Set up the agent and target
+		agent = GetComponent<NavMeshAgent>();
+		agent.speed = movementSpeed;
 
-        // Order infos based on distance threshold to make querying a bit faster.
+		// Order infos based on distance threshold to make querying a bit faster.
 		distancePollInfos = distancePollInfos.OrderBy(p => p.DistanceSqThreshold).ToArray();
 	}
 
@@ -42,17 +42,17 @@ public abstract class AbstractEnemy : MonoBehaviour
 
 	void Update()
 	{
-        UpdatePollInfo();
+		UpdatePollInfo();
 	}
 
 	void FixedUpdate()
-    {
-        // Check if enemy is in attack range to the player
-        inAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
+	{
+		// Check if enemy is in attack range to the player
+		inAttackRange = Physics.CheckSphere(transform.position, attackRange, isPlayer);
 
-        // Attack or Find player
-        if (inAttackRange) AttackPlayer();
-    }
+		// Attack or Find player
+		if (inAttackRange) AttackPlayer();
+	}
 
 	void OnDrawGizmosSelected()
 	{
@@ -62,29 +62,36 @@ public abstract class AbstractEnemy : MonoBehaviour
 		Gizmos.DrawWireSphere(agent.destination, 0.5f);
 	}
 
+	public void ResetPolling()
+	{
+		StopCoroutine(nameof(EnemyPolling));
+		StartCoroutine(nameof(EnemyPolling));
+	}
+
 	public virtual void AttackPlayer()
-    {
-        // If in range don't move and face player
-        transform.LookAt(targetPlayer.transform.position);
+	{
+		// If in range don't move and face player
+		transform.LookAt(targetPlayer.transform.position);
 
-        // Get health of player and attack with cooldown
-        HealthComponent targetHealth = targetPlayer.GetComponent<HealthComponent>();
-        if (targetHealth != null && !onCooldown)
-        {
-            targetHealth.TakeDamage(attackDamage);
-            onCooldown = true;
-            StartCoroutine(AttackCooldown());
-        }
-    }
+		// Get health of player and attack with cooldown
+		HealthComponent targetHealth = targetPlayer.GetComponent<HealthComponent>();
+		if (targetHealth != null && !onCooldown)
+		{
+			Debug.Log("Enemy attacking player!");
+			targetHealth.TakeDamage(attackDamage);
+			onCooldown = true;
+			StartCoroutine(AttackCooldown());
+		}
+	}
 
-    // Enemy Polling
-    IEnumerator EnemyPolling()
-    {
+	// Enemy Polling
+	IEnumerator EnemyPolling()
+	{
 		while (isAttacking)
 		{
 			// There is a chance that the coroutine could start before players are intialized. Wait a frame before processing.
 			if (targetPlayer is null)
-            {
+			{
 				yield return new WaitForEndOfFrame();
 			}
 			else
@@ -93,35 +100,33 @@ public abstract class AbstractEnemy : MonoBehaviour
 				yield return new WaitForSeconds(_distancePollInterval);
 			}
 		}
-    }
+	}
 
-    // Cooldown reset
-    IEnumerator AttackCooldown()
-    {
-        yield return new WaitForSeconds(attackCooldownTimer);
-        onCooldown = false;
-    }
+	// Cooldown reset
+	IEnumerator AttackCooldown()
+	{
+		yield return new WaitForSeconds(attackCooldownTimer);
+		onCooldown = false;
+	}
 
-    /// <summary>
-    /// Grabs distance poll info accordeing to nearest player. In addition, sets target player.
-    /// </summary>
+	/// <summary>
+	/// Grabs distance poll info accordeing to nearest player. In addition, sets target player.
+	/// </summary>
 	void UpdatePollInfo()
 	{
 		var newPollInterval = GetPollStateInfoFromDistance().PollInterval;
 		if (_distancePollInterval != newPollInterval)
 		{
 			_distancePollInterval = newPollInterval;
+			ResetPolling();
 		}
-		//ResetCoroutine
-		StopCoroutine(nameof(EnemyPolling));
-		StartCoroutine(nameof(EnemyPolling));
 	}
 
 	DistancePollObject GetPollStateInfoFromDistance()
 	{
-        (var player, var distanceSq) = FindNearestPlayer();
+		(var player, var distanceSq) = FindNearestPlayer();
 
-        targetPlayer = player;
+		targetPlayer = player;
 
 		DistancePollObject pollStateInfo = null;
 
