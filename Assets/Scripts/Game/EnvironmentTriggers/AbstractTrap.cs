07ceda1f102eae7b104b393e0e6b67b4bcd4b51b
Joshua Shigemitsu <jshigemitsu3@gatech.edu>
2024-09-20 17:32:10 -0700

    Changes to game scene and traps

--- Assets/Scripts/Game/EnvironmentTriggers/AbstractTrap.cs
+++ Assets/Scripts/Game/EnvironmentTriggers/AbstractTrap.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
 public abstract class AbstractTrap : MonoBehaviour
 {
     public TrapInfo trapInfo;
+    private List<Collider> trackedEnemies = new List<Collider>();
+    [SerializeField] private Animator myTrap = null;
 
     //TODO Replace activating OnTriggerEnter with player activating trap through interact button
 
@@ -11,8 +14,13 @@ public abstract class AbstractTrap : MonoBehaviour
     {
 
         if (collision.tag == "TestEnemyPool")
-        { 
-            trapInfo.isActive = true;  
+        {
+            trapInfo.isActive = true;
+            trackedEnemies.Add(collision);
+            if (trackedEnemies.Count == 1)
+            {
+                myTrap.SetTrigger("TriggerTrap");
+            }
         }
         else
         {
@@ -54,10 +62,31 @@ public abstract class AbstractTrap : MonoBehaviour
         {
             //Debug.Log("Enemy left trap");
             trapInfo.isActive = true;
+            trackedEnemies.Remove(collision);
+            if (trackedEnemies.Count == 0)
+            {
+                myTrap.SetTrigger("ResetTrap");
+            }
         }
         else
         {
             //Debug.Log("Player left Trap");
+        }
+    }
+    void Update()
+    {
+        for (int i = trackedEnemies.Count - 1; i >= 0; i--)
+        {
+            // if game object is disabled in hierarchy
+            if (!trackedEnemies[i].gameObject.activeInHierarchy) 
+            {
+                trackedEnemies.RemoveAt(i);
+                Debug.Log("Enemy destroyed while in trigger");
+            }
+            if (trackedEnemies.Count==0)
+            {
+                myTrap.SetTrigger("ResetTrap");
+            }
         }
     }
 }
