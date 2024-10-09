c6e02d4667521eb2f0db19947f6bb7023635f1ae
Joshua Shigemitsu <jshigemitsu3@gatech.edu>
2024-10-09 14:51:56 -0700

    commiting leveldesign

--- Assets/Scripts/Camera/FollowCam.cs
+++ Assets/Scripts/Camera/FollowCam.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;
+	public Transform target;
+	public Vector3 offset;
+	public float xMinBoundary;
+	public float xMaxBoundary;
 	public float followSpeed = 5.0f;
 	public bool lookAtTarget = true;
 
@@ -11,7 +13,14 @@ public class FollowCam : MonoBehaviour
 	{
 		if (target is null) return;
 
-		transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime * followSpeed);
+		Vector3 camPosition = target.position + offset;
+
+		float clampedX = Mathf.Clamp(camPosition.x, xMinBoundary, xMaxBoundary);
+
+
+		camPosition = new Vector3(clampedX, camPosition.y, camPosition.z);
+
+		transform.position = Vector3.Lerp(transform.position, camPosition, Time.deltaTime * followSpeed);
 		if (lookAtTarget)
 			transform.LookAt(target.position);
 	}
