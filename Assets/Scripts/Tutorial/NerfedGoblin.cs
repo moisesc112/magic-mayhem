49abe2bdcb58f2d8428181885b60467679cc00ac
samuel-carrasco <samuel.carrasco@smartthings.com>
2024-10-22 16:36:02 -0700

    enemies dont move/ are invincible/ dont do damage

---
+++ Assets/Scripts/Tutorial/NerfedGoblin.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityRandom = UnityEngine.Random;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NerfedNavPoller))]
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(HealthComponent))]
+[RequireComponent(typeof(Dissolver))]
+[RequireComponent(typeof(RagdollComponent))]
+[RequireComponent(typeof(LootDropComponent))]
+public class NerfedGoblin : MonoBehaviour
+{
+	[Header("Appearance")]
+	[SerializeField] GameObject[] _leftAccessories;
+	[SerializeField] GameObject[] _rightAccessories;
+	[SerializeField] GameObject[] _skins;
+	[SerializeField] float _accessoryChance = 0.25f;
+
+	[Header("Attack")]
+	[SerializeField] float _attackRange;
+	[SerializeField] float _damage;
+
+	void Awake()
+	{
+		_audioSource = GetComponent<AudioSource>();
+		_animator = GetComponent<Animator>();
+		_navPoller = GetComponent<NerfedNavPoller>();
+		_agent = GetComponent<NavMeshAgent>();
+		_healthComp = GetComponent<HealthComponent>();
+
+		_healthComp.onDeath += HealthComp_OnDeath;
+		_swingLayerIndex = _animator.GetLayerIndex("Combat");
+		_dissolver = GetComponent<Dissolver>();
+		_ragdoll = GetComponent<RagdollComponent>();
+		_lootDrop = GetComponent<LootDropComponent>();
+	}
+
+	void Start()
+	{
+		RandomizeLook();
+		_animator.SetInteger("SwingNum", UnityRandom.Range(1, 4));
+	}
+
+	void FixedUpdate()
+	{
+		if (!_healthComp.IsAlive)
+			return;
+
+		var distanceToPlayer = _navPoller.DistanceToPlayer;
+		if (distanceToPlayer <= _attackRange)
+		{
+			transform.LookAt(_navPoller.TargetPlayer?.GetAvatarPosition() ?? Vector3.zero);
+			if (!_isSwinging)
+				StartSwing();
+		}
+	}
+
+	private void OnDestroy()
+	{
+		_healthComp.onDeath -= HealthComp_OnDeath;
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, _attackRange);
+	}
+
+	/// <summary>
+	/// Called when spawned via pool
+	/// </summary>
+	public void InitFromPool(Vector3 location, Action<Goblin> releaseAction)
+	{
+		transform.position = location;
+		_agent.transform.position = transform.position;
+
+		enabled = true;
+		_navPoller.enabled = true;
+		_agent.enabled = true;
+
+		_dissolver.ResetEffect();
+		RandomizeLook();
+
+		_healthComp.health = _healthComp.maxHealth;
+		_ragdoll.DisableRagdoll();
+
+		_navPoller.StartPolling();
+
+		if (_releaseToPoolAction is null)
+			_releaseToPoolAction = releaseAction;
+	}
+
+	public void StartSwing()
+	{
+		_animator.SetInteger("SwingNum", UnityRandom.Range(1, 4));
+		_animator.SetBool("IsAttacking", true);
+		_animator.SetLayerWeight(_swingLayerIndex, 1.0f);
+		_isSwinging = true;
+		//_agent.updatePosition = false;
+	}
+
+	public void DoDamage()
+	{
+		_animator.SetBool("IsAttacking", false);
+		if (_navPoller.DistanceToPlayer <= _attackRange)
+			_navPoller.TargetPlayer.GetComponent<HealthComponent>().TakeDamage(_damage);
+	}
+
+	public void EndSwing()
+	{
+		_isSwinging = false;
+		_agent.updatePosition = true;
+		_animator.SetLayerWeight(_swingLayerIndex, 0.0f);
+	}
+
+	void RandomizeLook()
+	{
+		var randomSkin = UnityRandom.Range(0, _skins.Length);
+		var selectedSkin = _skins[randomSkin];
+		foreach (var skin in _skins.Where(s => s != selectedSkin))
+			skin.SetActive(false);
+
+		_dissolver.SetTargetRenderer(selectedSkin.GetComponent<Renderer>());
+
+		GetComponent<RagdollComponent>().SetBoundMesh(selectedSkin.transform);
+
+		selectedSkin.SetActive(true);
+
+		if (UnityRandom.Range(0.0f, 1.0f) > _accessoryChance)
+			_leftAccessories[UnityRandom.Range(0, _leftAccessories.Length)].SetActive(true);
+		if (UnityRandom.Range(0.0f, 1.0f) > _accessoryChance)
+			_rightAccessories[UnityRandom.Range(0, _rightAccessories.Length)].SetActive(true);
+	}
+
+	void HealthComp_OnDeath(object sender, EventArgs e)
+	{
+		StartCoroutine(nameof(HandleDeath));
+	}
+
+public event EventHandler onDeath;
+
+void HandleDeath()
+{
+    if (onDeath != null)
+    {
+        Debug.Log("Invoking onDeath");
+        onDeath.Invoke(this, EventArgs.Empty);
+    }
+    else
+    {
+        Debug.LogWarning("onDeath event is null.");
+    }
+}
+
+	AudioSource _audioSource;
+	Animator _animator;
+	NerfedNavPoller _navPoller;
+	NavMeshAgent _agent;
+	HealthComponent _healthComp;
+	Dissolver _dissolver;
+	RagdollComponent _ragdoll;
+	LootDropComponent _lootDrop;
+	Action<Goblin> _releaseToPoolAction;
+
+	bool _isSwinging;
+	int _swingLayerIndex;
+}
---
+++ Assets/Scripts/Tutorial/NerfedWarCheif.cs
@@ -0,0 +1,152 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityRandom = UnityEngine.Random;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(NerfedNavPoller))]
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(HealthComponent))]
+[RequireComponent(typeof(Dissolver))]
+[RequireComponent(typeof(RagdollComponent))]
+[RequireComponent(typeof(LootDropComponent))]
+public class NerfedWarCheif : MonoBehaviour
+{
+    [Header("Attack")]
+    [SerializeField] float _meleeRange;
+    [SerializeField] float _jumpRangeStart = 6.0f;
+    [SerializeField] float _jumpRangeEnd = 12.0f;
+    [SerializeField] float _damage;
+    [SerializeField] RadialDamageSource _jumpSource;
+    [SerializeField] Transform _hammerHitPoint;
+
+    [Header("FX")] 
+    [SerializeField] Renderer _renderer;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _animator = GetComponent<Animator>();
+        _navPoller = GetComponent<NerfedNavPoller>();
+        _agent = GetComponent<NavMeshAgent>();
+        _healthComp = GetComponent<HealthComponent>();
+        _dissolver = GetComponent<Dissolver>();
+        _dissolver.SetTargetRenderer(_renderer);
+        _ragdoll = GetComponent<RagdollComponent>();
+        _lootDrop = GetComponent<LootDropComponent>();
+
+        // No death handling
+    }
+
+    void Start()
+    {
+        _swingLayerIndex = _animator.GetLayerIndex("Combat");
+    }
+
+    private void FixedUpdate()
+    {
+        var distanceToPlayer = _navPoller.DistanceToPlayer;
+        // Check if in jumping sweet spot.
+        if (distanceToPlayer >= _jumpRangeStart && distanceToPlayer <= _jumpRangeEnd)
+        {
+            if (_canJump && !_isSwinging)
+                JumpAttack();
+        }
+
+        if (distanceToPlayer <= _meleeRange && !_isJumping)
+        {
+            transform.LookAt(_navPoller.TargetPlayer?.GetAvatarPosition() ?? Vector3.zero);
+            if (!_isSwinging)
+                MeleeAttack();
+        }
+    }
+
+    public void InitFromPool(Vector3 location, Action<WarChief> releaseAction)
+    {
+        Debug.Log("InitFromPool called for WarChief");
+
+        transform.position = location;
+        _agent.transform.position = transform.position;
+
+        enabled = true;
+        _navPoller.enabled = true;
+        _agent.enabled = true;
+
+        _canJump = true;
+
+        _dissolver.ResetEffect();
+
+        _healthComp.health = _healthComp.maxHealth;
+        _ragdoll.DisableRagdoll();
+
+        _navPoller.StartPolling();
+
+        if (_releaseToPoolAction == null)
+        {
+            _releaseToPoolAction = releaseAction;
+            Debug.Log("Assigned _releaseToPoolAction in InitFromPool for WarChief.");
+        }
+        else
+        {
+            Debug.Log("Already initialized _releaseToPoolAction for WarChief.");
+        }
+    }
+
+    public void JumpImpact()
+    {
+        Instantiate(_jumpSource, _hammerHitPoint.position, Quaternion.identity);
+    }
+
+    public void JumpEnd()
+    {
+        _canJump = true;
+        _animator.SetBool("Jump", false);
+        _agent.updatePosition = true;
+        _isJumping = false;
+    }
+
+    void JumpAttack()
+    {
+        _canJump = false;
+        _animator.SetBool("Jump", true);
+        _agent.updatePosition = false;
+        _isJumping = true;
+    }
+
+    void MeleeAttack()
+    {
+        _animator.SetInteger("SwingNum", UnityRandom.Range(1, 4));
+        _animator.SetBool("IsAttacking", true);
+        _animator.SetLayerWeight(_swingLayerIndex, 1.0f);
+        _isSwinging = true;
+    }
+
+    public void DoDamage()
+    {
+        _animator.SetBool("IsAttacking", false);
+        if (_navPoller.DistanceToPlayer <= _meleeRange)
+            _navPoller.TargetPlayer.GetComponent<HealthComponent>().TakeDamage(_damage);
+    }
+
+    public void EndSwing()
+    {
+        _isSwinging = false;
+        _agent.updatePosition = true;
+        _animator.SetLayerWeight(_swingLayerIndex, 0.0f);
+    }
+
+    AudioSource _audioSource;
+    Animator _animator;
+    NerfedNavPoller _navPoller;
+    NavMeshAgent _agent;
+    HealthComponent _healthComp;
+    Dissolver _dissolver;
+    RagdollComponent _ragdoll;
+    LootDropComponent _lootDrop;
+    Action<WarChief> _releaseToPoolAction;
+
+    bool _canJump;
+    bool _isJumping;
+    bool _isSwinging;
+    int _swingLayerIndex;
+}
