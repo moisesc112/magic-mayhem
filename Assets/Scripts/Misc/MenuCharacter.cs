fd231394dca738c3bf96ce640340d41e493eefd9
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2024-10-23 23:53:12 -0700

    Updated menu to use a 3d environment and simpler lobby system.

---
+++ Assets/Scripts/Misc/MenuCharacter.cs
@@ -0,0 +1,58 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.UI;
+
+[RequireComponent(typeof(Animator))]
+public class MenuCharacter : MonoBehaviour
+{
+	[Header("UI")]
+	[SerializeField] TextMeshProUGUI _joinText;
+	[SerializeField] TextMeshProUGUI _playerText;
+
+	[Header("Config")]
+	[SerializeField] int _playerIndex;
+	[SerializeField] int _idlePose = 1;
+	[SerializeField] InputSystemUIInputModule _inputModule;
+	public InputSystemUIInputModule inputModule => _inputModule;
+
+
+	public int playerIndex => _playerIndex;
+
+	private void Awake()
+	{
+		_anim = GetComponent<Animator>();
+	}
+
+	private void Start()
+	{
+		_anim.SetInteger("Pose", _idlePose);
+	}
+
+	private void OnDestroy()
+	{
+		_inputModule.cancel.action.performed -= OnCancel;
+	}
+
+	public void Join()
+	{
+		_playerText.gameObject.SetActive(true);
+		_joinText.gameObject.SetActive(false);
+		_inputModule.cancel.action.performed += OnCancel;
+	}
+
+	public void BackOut()
+	{
+		_playerText.gameObject.SetActive(false);
+		_joinText.gameObject.SetActive(true);
+		_inputModule.cancel.action.performed -= OnCancel;
+	}
+
+	void OnCancel(InputAction.CallbackContext context)
+	{
+		BackOut();
+		PlayerManager.instance.RemovePlayer(_playerIndex);
+	}
+
+	Animator _anim;
+}
