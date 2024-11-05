c88315590e7c7626a7913bca15e2aa5fd071fa0e
Moises Castro <mcastro60@gatech.edu>
2024-11-05 14:52:48 -0800

    Added MeteorStrike Spell

---
+++ Assets/Scripts/Player/Abilities/MeteorStrike/MeteorStrike.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorStrike : Ability
+{
+    [SerializeField] AudioSource audioSource;
+    private bool isAudioPaused;
+    private Coroutine audioCoroutine;
+
+    public override void Awake()
+    {
+        audioCoroutine = StartCoroutine(AudioFadeOut.FadeOut(audioSource, abilityInfo.despawnTime));
+        StartCoroutine(EnableHitBox());
+        base.Awake();
+    }
+
+    public override void UpdateProjectileVelocity() { }
+
+    IEnumerator EnableHitBox()
+    {
+        yield return new WaitForSeconds(0.4f);
+        transform.GetComponent<SphereCollider>().enabled = true;
+        yield return new WaitForSeconds(0.1f);
+        transform.GetComponentInChildren<SphereCollider>().enabled = false;
+    }
+
+    public override void Update()
+    {
+        if (Time.deltaTime == 0)
+        {
+            audioSource.Pause();
+            StopCoroutine(audioCoroutine);
+            isAudioPaused = true;
+        }
+        else
+        {
+            if (isAudioPaused == true)
+            {
+                audioSource.UnPause();
+                audioCoroutine = StartCoroutine(AudioFadeOut.FadeOut(audioSource, remainingDespawnTime));
+                isAudioPaused = false;
+            }
+        }
+        base.Update();
+    }
+    public override void OnTriggerEnter(Collider collision)
+    {
+        if (collision != null && collision.tag != "Player" && collision.GetComponent<HealthComponent>() != null)
+        {
+            collision.GetComponent<HealthComponent>().TakeDamage(GetAbilityDamage());
+        }
+    }
+}
