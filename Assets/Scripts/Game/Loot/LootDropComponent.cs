b9e9a7b20a8c70ed493ef94bfbe4bf0496af670c
Moises Castro <mcastro60@gatech.edu>
2024-11-04 20:48:28 -0800

    Lootable Health Potion and Breakable Barrel

--- Assets/Scripts/Game/Loot/LootDropComponent.cs
+++ Assets/Scripts/Game/Loot/LootDropComponent.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class LootDropComponent : MonoBehaviour
 {
-    [SerializeField] LootInfo lootInfo;
+	[SerializeField] LootInfo lootInfo;
 
-    public void DropLoot()
-    {
-        var loot = Instantiate(lootInfo.lootGameObject, transform.position, Quaternion.identity);
-        loot.GetComponent<Coin>().SetValue(lootInfo.amount);
-    }
+	public void DropLoot()
+	{
+		var loot = Instantiate(lootInfo.lootGameObject, transform.position, Quaternion.identity);
+		if(loot.GetComponent<Coin>() != null) 
+			loot.GetComponent<Coin>().SetValue(lootInfo.amount);
+		if (loot.GetComponent<HealthPotion>() != null)
+			loot.GetComponent<HealthPotion>().SetValue(lootInfo.amount);
+	}
 }
