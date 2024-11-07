5a426f5865d76fde567acd240df8cca1deb9d2f6
DavidFuentes4224 <davidfuentes4224@yahoo.com>
2024-11-06 21:29:38 -0800

    Added mystic boomerang spell.

--- Assets/Scripts/Game/Components/AdvancedProjectileMotion.cs
+++ Assets/Scripts/Game/Components/AdvancedProjectileMotion.cs
@@ -3,53 +3,66 @@ using UnityEngine;
 
 public class AdvancedProjectileMotion : MonoBehaviour
 {
-    public bool useX;
+	public bool useX;
 	public AnimationCurve deltaX;
-    public bool useY;
+	public bool useY;
 	public AnimationCurve deltaY;
-    public bool useZ;
+	public bool useZ;
 	public AnimationCurve deltaZ;
+	public bool useWorldPos = true;
 
-    public float timeTilCompletion = 1.0f;
+	public float timeTilCompletion = 1.0f;
 
 	private void Awake()
 	{
-        _dmg = GetComponent<DamageIndicator>();
+		_dmg = GetComponent<DamageIndicator>();
 	}
 
 	// Start is called before the first frame update
 	void Start()
-    {
-        originalPos = transform.position;
-    }
-
-    public void StartMoving()
-    {
-        t = 0;
+	{
 		originalPos = transform.position;
-        StartCoroutine(nameof(DoMove));
-    }
-
-    IEnumerator DoMove()
-    {
-        while (t < timeTilCompletion)
-        {
-			t += Time.deltaTime;
-            var timeLinePos = t / timeTilCompletion;
-			Vector3 targetPos = originalPos;
-			if (useX) targetPos.x += deltaX.Evaluate(timeLinePos);
-            if (useY) targetPos.y += deltaY.Evaluate(timeLinePos);
-			if (useZ) targetPos.z += deltaZ.Evaluate(timeLinePos);
-			transform.position = targetPos;
-			yield return new WaitForEndOfFrame();
-        }
-        if (_dmg)
-            NotificationCenter.instance.damageTextFinishedMoving?.Invoke(this, new GenericEventArgs<DamageIndicator>(_dmg));
-
-        transform.position = originalPos;
 	}
 
-    DamageIndicator _dmg;
-    float t;
-    Vector3 originalPos;
+	public void StartMoving()
+	{
+		t = 0;
+		originalPos = transform.position;
+		StartCoroutine(nameof(DoMove));
+	}
+
+	IEnumerator DoMove()
+	{
+		while (t < timeTilCompletion)
+		{
+			t += Time.deltaTime;
+			var timeLinePos = t / timeTilCompletion;
+			if (useWorldPos)
+			{
+				Vector3 targetPos = originalPos;
+				if (useX) targetPos.x += deltaX.Evaluate(timeLinePos);
+				if (useY) targetPos.y += deltaY.Evaluate(timeLinePos);
+				if (useZ) targetPos.z += deltaZ.Evaluate(timeLinePos);
+				transform.position = targetPos;
+			}
+			else
+			{
+				Vector3 delta = Vector3.zero; ;
+				if (useX) delta.x += deltaX.Evaluate(timeLinePos);
+				if (useY) delta.y += deltaY.Evaluate(timeLinePos);
+				if (useZ) delta.z += deltaZ.Evaluate(timeLinePos);
+				
+				transform.position =  originalPos + transform.TransformDirection(delta);
+			}
+			yield return null;
+		}
+		if (_dmg)
+			NotificationCenter.instance.damageTextFinishedMoving?.Invoke(this, new GenericEventArgs<DamageIndicator>(_dmg));
+
+		transform.position = originalPos;
+	}
+
+	DamageIndicator _dmg;
+	float t;
+	Vector3 originalPos;
 }
