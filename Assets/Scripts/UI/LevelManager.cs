eb87ecadec5f5ab69c15745882a9003e28c1c728
Joshua Shigemitsu <jshigemitsu3@gatech.edu>
2024-11-18 23:00:09 -0800

    Added out of bounds fog

--- Assets/Scripts/UI/LevelManager.cs
+++ Assets/Scripts/UI/LevelManager.cs
@@ -11,16 +11,27 @@ public sealed class LevelManager : Singleton<LevelManager>
     public float zMaxBoundary;
     public event System.Action UpdateBoundary;
 
+
+    public GameObject shop;
     public GameObject plainsBoundary;
     public GameObject goblinBoundary;
+    public GameObject westFog;
+    public GameObject eastFog;
+    public GameObject northFog;
+    public GameObject southFog;
+    
 
+    public Vector3 shopPlains;
+    public Vector3 shopGoblins;
 
+    private Animator westFogAnimator;
     private int waveCounter = 1;
     public int plainsLevelWaveStart;
     public int goblinsLevelWaveStart;
 
     void Start()
     {
+        westFogAnimator = westFog.GetComponent<Animator>();
         if (WaveManager.instance is null || plainsBoundary is null || goblinBoundary is null) return;
         WaveManager.instance.waveFinished += WaveManager_WaveFinished;
         WaveManager.instance.waveStarted += WaveManager_WaveStarted;
@@ -35,16 +46,19 @@ public sealed class LevelManager : Singleton<LevelManager>
 
     private void WaveManager_WaveStarted(object sender, WaveStartedEventArgs e)
     {
-        if (waveCounter == plainsLevelWaveStart)
+        if (waveCounter == plainsLevelWaveStart || waveCounter == goblinsLevelWaveStart)
         {
-            plainsBoundary.SetActive(true);
-            xMinBoundary += 75;
-            BoundsUpdated();
-        }
-        else if (waveCounter == goblinsLevelWaveStart)
-        {
-            goblinBoundary.SetActive(true);
-            xMinBoundary += 75;
+            if (waveCounter == plainsLevelWaveStart)
+            {
+                plainsBoundary.SetActive(true);
+                eastFog.transform.position = new Vector3(eastFog.transform.position.x + 74.07f, eastFog.transform.position.y, eastFog.transform.position.z);
+            }
+            else
+            {
+                goblinBoundary.SetActive(true);
+                eastFog.transform.position = new Vector3(eastFog.transform.position.x + 74.07f, eastFog.transform.position.y, eastFog.transform.position.z);
+            }
+             xMinBoundary += 75;
             BoundsUpdated();
         }
     }
@@ -52,15 +66,22 @@ public sealed class LevelManager : Singleton<LevelManager>
     private void WaveManager_WaveFinished(object sender, WaveEndedEventArgs e)
     {
         waveCounter++;
-        if (waveCounter == plainsLevelWaveStart)
+        if (waveCounter == plainsLevelWaveStart || waveCounter == goblinsLevelWaveStart)
         {
-            plainsBoundary.SetActive(false);
-            xMaxBoundary += 75;
-            BoundsUpdated();
-        }
-        else if (waveCounter == goblinsLevelWaveStart)
-        {
-            goblinBoundary.SetActive(false);
+            if (waveCounter == plainsLevelWaveStart)
+            {
+                plainsBoundary.SetActive(false);
+                shop.transform.position = shopPlains;
+                westFogAnimator.SetTrigger("TriggerSecondLevel");
+            }
+            else
+            {
+                goblinBoundary.SetActive(false);
+                shop.transform.position = shopGoblins;
+                northFog.transform.position = new Vector3(northFog.transform.position.x + 50, northFog.transform.position.y, northFog.transform.position.z);
+                southFog.transform.position = new Vector3(southFog.transform.position.x + 50, southFog.transform.position.y, southFog.transform.position.z);
+                westFogAnimator.SetTrigger("TriggerThirdLevel");
+            }
             xMaxBoundary += 75;
             BoundsUpdated();
         }
